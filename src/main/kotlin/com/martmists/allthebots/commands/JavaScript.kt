bf12a9e656aeec1d7d8170806d835cb19ba35853
Martmists <martmists@gmail.com>
2018-08-18 15:19:26 +0200

    Javascript code execution
    Signed-off-by: Martmists <martmists@gmail.com>

---
+++ src/main/kotlin/com/martmists/allthebots/commands/JavaScript.kt
@@ -0,0 +1,48 @@
+package com.martmists.allthebots.commands
+
+import com.martmists.chitose.entities.cmd.Command
+import com.martmists.chitose.entities.cmd.CommandContext
+import com.martmists.chitose.entities.cmd.argument
+import com.martmists.chitose.entities.permissions.UserPermission
+import jdk.nashorn.api.scripting.ClassFilter
+import jdk.nashorn.api.scripting.NashornScriptEngineFactory
+import net.dv8tion.jda.core.Permission
+import javax.script.ScriptContext
+import javax.script.SimpleScriptContext
+
+class JavaScript: Command() {
+    override val description = "Run JavaScript code"
+    override val example = "javascript print('abc');"
+    override val usage = "javascript <code>"
+
+    init {
+        aliases += "js"
+
+        userPermissions += UserPermission(Permission.MANAGE_SERVER)
+
+        arguments += argument<String>("code")
+    }
+
+    class CF: ClassFilter {
+        override fun exposeToScripts(p0: String?): Boolean {
+            return false
+        }
+    }
+
+    override fun run(ctx: CommandContext) {
+        val code = ctx.args["code"] as String
+
+        val engine = NashornScriptEngineFactory().getScriptEngine(CF())
+        val context = SimpleScriptContext().apply {
+            setAttribute("ctx", ctx, ScriptContext.ENGINE_SCOPE)
+            setAttribute("jda", ctx.jda, ScriptContext.ENGINE_SCOPE)
+        }
+
+        try {
+            val result = engine.eval(code, context)
+            ctx.send(result.toString())
+        } catch (e: Exception) {
+            ctx.sendException(e)
+        }
+    }
+}
